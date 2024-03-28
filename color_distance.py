fae34780a4a41c9245c0c403e97261da0e84fe08
wesomalley <123903000+wesomalley@users.noreply.github.com>
2024-03-28 14:27:00 -0500

    Update color_distance.py
    logic update

--- color_distance.py
+++ color_distance.py
@@ -23,7 +23,50 @@ def detect_largest_color(mask, color_name, color_frame):
         return (x + w // 2, y + h // 2)
     else:
         return None
+    
+def course_correct(point, center_x):
+    print("course correct")
+    # check if the detected point is not in the center of the page with a threshold of 15 pixels
+    while (point[0] <= center_x-15) or (point[0] >= center_x+15):
+        #check if bucket is to the right or left of center and adjust
+        if point[0] >= center_x:
+            print("turn right") # put uart command here
+        if point[0] <= center_x:
+            print("turn left") # put uart command here
+    
+def blue_bucket_function(point, center_x, distance):
+    print("blue test")
+    course_correct(point, center_x)
+    
+    while distance > 1:
+        print("go forward") # put uart command here
 
+    print("stop") # put uart command here
+    print("turn left") # put uart command here
+    print("go forward 2 meters") # put uart command here
+    print("turn right") # put uart command here
+    print("go forward until next obstacle") # put uart command here
+
+
+def yellow_bucket_function(point, center_x, distance):
+    print("yellow test")
+    course_correct(point, center_x)
+
+    while distance > 1:
+        print("go forward") # put uart command here
+
+    print("stop") # put uart command here
+    print("turn left") # put uart command here
+    print("go forward 2 meters") # put uart command here
+    print("turn right") # put uart command here
+    print("go forward until next obstacle") # put uart command here
+
+def red_bucket_function(point, center_x, distance):
+    print("red test")
+    course_correct(point, center_x)
+    print("go forward") # put uart command here
+
+    
 # Capturing webcam footage
 dc = DepthCamera()
 
@@ -33,6 +76,10 @@ cv2.setMouseCallback("Color frame", show_distance)
 while True:
     ret, depth_frame, color_frame = dc.get_frame()  # Reading webcam footage
     img = cv2.cvtColor(color_frame, cv2.COLOR_BGR2HSV)  # Converting BGR image to HSV format
+
+    # calculate center of image for course correction
+    height, width, _ = img.shape
+    center_x = width // 2
 
     points = []
 
@@ -46,14 +93,14 @@ while True:
 
     red_point = detect_largest_color(red_mask, "Red", color_frame)
     blue_point = detect_largest_color(blue_mask, "Blue", color_frame)
-    # yellow_point = detect_largest_color(yellow_mask, "Yellow", color_frame)
+    yellow_point = detect_largest_color(yellow_mask, "Yellow", color_frame)
 
     if red_point:
         points.append(red_point)
     if blue_point:
         points.append(blue_point)
-    # if yellow_point:
-    #     points.append(yellow_point)
+    if yellow_point:
+         points.append(yellow_point)
 
     for point in points:
         cv2.circle(color_frame, point, 4, (0, 0, 255))
@@ -63,4 +110,17 @@ while True:
 
     cv2.imshow("Color frame", color_frame)  # Displaying webcam image
 
+    # check which color is being detected and call appropriate function
+    for point in points:
+        distance = depth_frame[point[1], point[0]]
+        if point == blue_point:
+            blue_bucket_function(point, center_x, distance)
+        if point == yellow_point:
+            yellow_bucket_function(point, center_x, distance)
+        if point == red_point:
+            red_bucket_function(point, center_x, distance)
+
+
     cv2.waitKey(1)
+
+

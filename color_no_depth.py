86b5b2b6462d13681817be7315a933c84c8ba18e
teaganrm <112922785+teaganrm@users.noreply.github.com>
2024-04-16 14:42:13 -0500

    changed color_no_depth

--- color_no_depth.py
+++ color_no_depth.py
@@ -38,10 +38,11 @@ while True:
 
     points = []
 
-    lower_red1, upper_red1 = np.array([168, 100, 100]), np.array([180, 255, 255])  # Red color range
-    lower_red2, upper_red2 = np.array([0, 200, 200]), np.array([10, 255, 255])  # Red color range
+    lower_red1, upper_red1 = np.array([0, 100, 100]), np.array([10, 255, 255])  # Red color range
+    lower_red2, upper_red2 = np.array([170, 100, 100]), np.array([180, 255, 255])  # Red color range
     lower_blue, upper_blue = np.array([95, 70, 70]), np.array([130, 255, 255])  # Blue color range
-    lower_yellow, upper_yellow = np.array([20, 65, 65]), np.array([40, 255, 255])  # Yellow color range
+    lower_yellow, upper_yellow = np.array([20, 100, 100]), np.array([40, 255, 255])  # Yellow color range
+    lower_wood, upper_wood = np.array([12, 50, 100]), np.array([40, 200, 200])
 
     lower_red_combined = np.minimum(lower_red1, lower_red2)
     upper_red_combined = np.maximum(upper_red1, upper_red2)
@@ -49,10 +50,12 @@ while True:
     red_mask = cv2.inRange(img, lower_red_combined, upper_red_combined)
     blue_mask = cv2.inRange(img, lower_blue, upper_blue)
     yellow_mask = cv2.inRange(img, lower_yellow, upper_yellow)
+    wood_mask = cv2.inRange(img, lower_wood, upper_wood)
 
     red_point = detect_largest_color(red_mask, "Red", color_frame)
     blue_point = detect_largest_color(blue_mask, "Blue", color_frame)
     yellow_point = detect_largest_color(yellow_mask, "Yellow", color_frame)
+    wood_point = detect_largest_color(wood_mask, "Wood", color_frame)
 
     if red_point:
         points.append(red_point)
@@ -60,8 +63,11 @@ while True:
         points.append(blue_point)
     elif yellow_point:
         points.append(yellow_point)
+    elif wood_point:
+        points.append(wood_point)
     else: 
         points.append((color_frame.shape[1] // 2, color_frame.shape[0] // 2))
+
 
     for point in points:
         cv2.circle(color_frame, point, 4, (0, 0, 255))
